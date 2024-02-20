2dda06c0aa867101282827d013d710ad1c300a92
MA. L <matelop132@gmail.com>
2024-02-20 10:11:10 +0100

    Account Create Page add and refact

--- resources/views/accountCreate.blade.php
+++ resources/views/accountCreate.blade.php
@@ -3,14 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="css/accountCreate.css" rel="stylesheet">
+
+    <title>Créer ton Compte</title>
 </head>
 <body>
         <header>
             <iframe src="/header" width="100%" height="100"></iframe>
         </header>
         <main>
-            
+            <div>
+            <form type="POST">
+                <div>
+                <input value="Nom"type="text"> <br>
+                <input value="Prenom" type="text"> <br>
+                <input value="Adresse Email" type='mail'> <br>
+                <input value="Mot de passe" type="text"> <br>
+                <input value="Confirmation mot de passe" type="text"> <br>
+                </div>
+                <input type='button' value='Enregistrer les modifications' class="buttonPostCreateAcccount">
+            </form>
+            </div>
         </main>
         <footer>
         <iframe src="/footer" width="100%" height="100"></iframe>
